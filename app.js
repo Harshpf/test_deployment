4b801f7c91953e499c4a6e138ef56e91e8023a67
Priyanshu pandey <priyanshupandeyuse111@gmail.com>
2025-08-27 23:49:58 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/api/hello', (req, res) => {
+    res.json({ message: 'Hello, world!' });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
